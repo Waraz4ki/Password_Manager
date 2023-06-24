68616eba1307f2acd644988d207fd242955474e0
Moritz Balogh <Waraz4ki@gmail.com>
2023-06-24 17:51:21 +0200

    Importing issue resolved

--- src/new_entry.py
+++ src/new_entry.py
@@ -1,12 +1,17 @@
-
+from encryption import file_encryptor
 
 title = input("Title: ")
 username = input("Username: ")
 password = input("Password: ")
 
-def write_entry():
-    with open("data\data", "a") as file:
+def write_entry(save_file):
+    with open(save_file, "a") as file:
         file.write(title+username+password)
+
+
+write_entry("moritz.kd")
+file_encryptor.encrypt_file("moritz.kd", "data/encryption_key.key")
+
 
     
     
