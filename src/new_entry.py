59c25cbe1483b366192bae8f7c58d7a443492314
Moritz Balogh <Waraz4ki@gmail.com>
2023-08-21 13:49:37 +0200

    New Entry can now enrypt things

--- src/new_entry.py
+++ src/new_entry.py
@@ -1,16 +1,23 @@
 from encryption import file_encryptor
+from cryptography.fernet import Fernet
 
-title = input("Title: ")
-username = input("Username: ")
-password = input("Password: ")
+encoding = "utf-8"
+key = "lxp0wlS5yygvg2jyl1AS3toNKvhewzhulAmMMPyj0Co="
+
+encrypt = Fernet(key).encrypt
+
+title = encrypt((input("Title: ").encode(encoding)))
+print(Fernet(key).decrypt(title))
+username = encrypt((input("Name: ").encode(encoding)))
+password = encrypt((input("Pass: ").encode(encoding)))
+
 
 def write_entry(save_file):
     with open(save_file, "a") as file:
-        file.write(title+username+password)
+        file.write(title.decode(encoding))
+        file.write(username.decode(encoding))
+        file.write(password.decode(encoding))
 
-
-write_entry("moritz.kd")
-file_encryptor.encrypt_file("moritz.kd", "data/encryption_key.key")
 
 
     
