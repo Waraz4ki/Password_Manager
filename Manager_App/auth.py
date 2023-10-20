581a2e08e1d4d69a6dfe6bc5dcb51f73f4df7eb3
Moritz Balogh <Waraz4ki@gmail.com>
2023-10-20 17:15:23 +0200

    IDK

--- Manager_App/auth.py
+++ Manager_App/auth.py
@@ -2,36 +2,28 @@ import os
 import sys
 import hashlib
 
-from Manager_App.models import Base, Entries, Groups
+from Manager_App.models import Base, Entry, Group
 from flask import Blueprint, render_template, request, flash, redirect, url_for, get_flashed_messages
 from sqlalchemy import create_engine, MetaData
 
 
 auth = Blueprint("auth", __name__)
-DB_NAME = "moritz"
-engine = create_engine(f"sqlite:///data/{DB_NAME}.db")
-
-@auth.route("/view")
-def test():
-    return render_template("file_view.html")
 
 @auth.route("/test")
 def test2():
     return render_template("organizethis.html")
+
+def __create_engine__(__dbname__):
+    engine = create_engine(f"sqlite:///data/{__dbname__}.db")
+    return engine
     
 @auth.route("/", methods=["GET", "POST"])
 def open_database():
     if request.method == "POST":
         try:
             #! Look at Bcrypt or Argon2
-            DATABASE = request.form.get("DATABASE")
+            DB_NAME = request.form.get("DATABASE")
             masterKEY = hashlib.sha256(request.form.get("masterKEY").encode(), usedforsecurity=True).hexdigest()
-#            db = DB.DBClass(DATABASE)
-#            if db.openingCheck() == masterKEY:
-#                return redirect(url_for("views.file_view"))
-#            else:
-#                flash("Master Key is Wrong Please check your spelling", category="error")
-
         except FileNotFoundError:
             flash("Database doesn't exsit", category="error")
     return render_template("index.html")
@@ -39,11 +31,15 @@ def open_database():
 @auth.route("/createDatabase", methods=["GET", "POST"])
 def create_database():
     if request.method == "POST":
-        DATABASE = request.form.get("DATABASE")
-        if os.path.exists(DATABASE) is True:
+        DB_NAME = request.form.get("DATABASE")
+        if os.path.exists(f"data/{DB_NAME}"):
             flash("Database already exists")
         else:
             masterKEY = hashlib.sha256(request.form.get("masterKEY").encode(), usedforsecurity=True).hexdigest()
-            print(masterKEY)
-            Base.metadata.create_all(engine)
+            Base.metadata.create_all(__create_engine__(__dbname__=DB_NAME))
+            return redirect(url_for("auth.open_database"))
     return render_template("createDatabase.html")
+
+
+    
+
--- Manager_App/models.py
+++ Manager_App/models.py
@@ -1,52 +1,60 @@
 import sqlalchemy
 from typing import Any, Optional, List
 from sqlalchemy import MetaData, create_engine
+from sqlalchemy import insert, update, delete, select
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
-from sqlalchemy import Table, Column, Integer, String, ForeignKey, DATETIME, DATE
+#from sqlalchemy import Table, Column, Integer, String, ForeignKey, DATETIME, DATE
 
 metadata_obj = MetaData()
-engine = create_engine(f"sqlite:///data/asda.db")
+engine = create_engine(f"sqlite:///data/asda.db", echo=True)
 
 class Base(DeclarativeBase):
     pass
 
-#class Entries(Base):
-#    __tablename__ = "Entries"
-#    id = Column("id", Integer, primary_key=True)
-#    title = Column("title", String())
-#    password = Column("password", String())
-#    notes =  Column("notes", String())
-#    assigned_group = Column("assigned_group", ForeignKey("Groups.id"))
-#    date = Column(DATETIME)
-#
-#class Groups(Base):
-#    __tablename__ = "Groups"
-#    id = Column("id", Integer, primary_key=True)
-#    group_name = Column("group_name", String())
-    
+
+class Config(Base):
+    __tablename__ = "Config"
+
+    masterKEY : Mapped[str] = mapped_column(primary_key=True)
+
+    def __repr__(self) -> str:
+        return f"{self.masterKEY}"    
+
+
 class Entry(Base):
     __tablename__ = "Entry"
-    
+
     id : Mapped[int] = mapped_column(primary_key=True)
-    title : Mapped[str]
-    password : Mapped[str]
-    notes : Mapped[str]
-    assigned_group = mapped_column(ForeignKey("Group.id"))
-    
+    title : Mapped[Optional[str]]
+    password : Mapped[Optional[str]]
+    notes : Mapped[Optional[str]]
+    #assigned_group : Mapped["Group"] = relationship(back_populates="assigned_entries")
+
     def __repr__(self) -> str:
-        return f"{self.id}, {self.title}, {self.password}, {self.notes}, {self.assigned_group}"
-    
+        return f"{self.id}, {self.title}, {self.password}, {self.notes}"
+
+
 class Group(Base):
     __tablename__ = "Group"
     
     id : Mapped[int] = mapped_column(primary_key=True)
-    group_name : Mapped[str]
-    
+    group_name : Mapped[Optional[str]]
+    #assigned_entries : Mapped[Entry] = relationship(back_populates="assigned_group")
+
     def __rep__(self) -> str:
         return f"{self.id}, {self.group_name}"
 
 
-#? Use this to create all the defined tables al a 'class ...(Base):'
-#! Must use Subclass of DeclarativeBase like above, see: sqlalchemy.exc.InvalidRequestError: Cannot use "DeclarativeBase" directly as a declarative base class. Create a Base by creating a subclass of it
-asd = Entry()
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    #? Use this to create all the defined tables al a 'class ...(Base):'
+    #! Must use Subclass of DeclarativeBase like above, see: sqlalchemy.exc.InvalidRequestError: Cannot use "DeclarativeBase" directly as a declarative base class. Create a Base by creating a subclass of it
+    #Base.metadata.create_all(engine, checkfirst=False)
+    enter = insert(Entry).values(title="DSB",password="1234")
+    get = select(Entry)
+
+    with engine.begin() as be:
+        #be.execute(insert(Entry).values(id=1872435))
+        be.execute(update(Entry).where(Entry.id==1872435).values(title="Servus", password="1234"))
+        #be.execute(enter)
+        #print(be.execute(enter).inserted_primary_key)
+        #be.execute(get)
