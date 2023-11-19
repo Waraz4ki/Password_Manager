1e9bcfd242885a10a19b8a0775ea65bd58692ba5
Moritz Balogh <Waraz4ki@gmail.com>
2023-11-19 13:13:38 +0100

    Refactored

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def unauthorized(error_code):
     print("401")
     return render_template("error_base.html", error = error_code)
 
-#@app.errorhandler(404)
-#def page_not_found(error_code):
-#    print("404")
-#    return render_template("error_base.html", error = error_code)
+@app.errorhandler(404)
+def page_not_found(error_code):
+    print("404")
+    return render_template("error_base.html", error = error_code)
 
 #TODO Find out if you can use tkinterer to display all the localhost stuff(html and css works I looked ut up)
 if __name__ == '__main__':
